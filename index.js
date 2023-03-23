c30e627e19f9af586d68bc756938949e5e312cd0
DICKSON MUTWIRI GUANTAI <dicksonguantai@gmail.com>
2023-03-24 01:08:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+//Creates avariable pointing to element main with id #main
+const main = document.getElementById("main");
+
+main.remove();// deletes variable main that points to element main
+
+const newHeader = document.createElement('h1');
+
+// Set the id attribute to "victory"
+newHeader.setAttribute('id', 'victory');
+
+// Add text to the h1 element
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Append the h1 element to the body of the DOM
+document.body.appendChild(newHeader);
